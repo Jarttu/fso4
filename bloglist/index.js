6fd95bcca7bd2427d31a101aa514a04b06ca28f4
Jarttu <Jaahas.ihminen@gmail.com>
2026-04-24 02:02:35 +0300

    4.2

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,37 +1,14 @@
-const express = require('express')
 const mongoose = require('mongoose')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const app = require('./app')
 
 const mongoUrl = 'mongodb://Jarttu:bloglist@ac-h8pma42-shard-00-00.ax2t2ja.mongodb.net:27017,ac-h8pma42-shard-00-01.ax2t2ja.mongodb.net:27017,ac-h8pma42-shard-00-02.ax2t2ja.mongodb.net:27017/bloglist?ssl=true&replicaSet=atlas-354lx8-shard-0&authSource=admin&retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { family: 4 })
 
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+mongoose.connect(mongoUrl)
+	.then(() => console.log('connected to MongoDB'))
+	.catch(err => console.log(err))
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
