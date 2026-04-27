315453071a5383b014f30eeea7fe48c86035f92d
Jarttu <Jaahas.ihminen@gmail.com>
2026-04-27 18:04:39 +0300

    4.8

---
+++ bloglist/tests/blog_api.test.js
@@ -0,0 +1,49 @@
+const { test, describe, before, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const mongoUrl = 'mongodb://Jarttu:bloglist@ac-h8pma42-shard-00-00.ax2t2ja.mongodb.net:27017,ac-h8pma42-shard-00-01.ax2t2ja.mongodb.net:27017,ac-h8pma42-shard-00-02.ax2t2ja.mongodb.net:27017/bloglist?ssl=true&replicaSet=atlas-354lx8-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+before(async () => {
+    await mongoose.connect(mongoUrl)
+})
+
+describe('blog api', () => {
+
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+
+        const blog = new Blog({
+            title: 'Test blog',
+            author: 'Tester',
+            url: 'http://example.com',
+            likes: 5
+        })
+
+        await blog.save()
+    })
+
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+
+        assert.strictEqual(response.body.length, 1)
+    })
+
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
